5c77c8fe7ffd483ab6be050233ecbc6118f87705
LuuKhang2004 <150054154+LuuKhang2004@users.noreply.github.com>
2024-12-10 21:29:14 +0700

    Add files via upload
    add dijtra to main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from graphgen import generate_connected_graph
-from smastar import SMAStar
+# from smastar import SMAStar
+from solve import dijkstra, read_graph_from_list
 import time
 # Sinh đồ thị
 V = 6
 min_weight, max_weight = 1, 10
 graph = generate_connected_graph(V, min_weight, max_weight)
-
+print(type(graph[0]))
 print("Danh sách cạnh của đồ thị:")
 for edge in graph:
     print(edge)
@@ -28,6 +29,13 @@ else:
 
 print(f"Thời gian thực thi: {execution_time:.6f} giây")
 
+graph_dijkstra = read_graph_from_list(graph)
+start_time = time.time()  # Thời gian bắt đầu
+dijkstra_path, dijkstra_cost = dijkstra(graph_dijkstra, start, goal)
+end_time = time.time()  # Thời gian kết thúc
+execution_time = end_time - start_time # Tính thời gian thực thi
 
-
+print(f"Chi phí ngắn nhất từ {start} đến {goal} (Dijkstra): {dijkstra_cost}")
+print(f"Đường đi ngắn nhất từ {start} đến {goal} (Dijkstra): {dijkstra_path}")
+print(f"Thời gian thực thi (Dijkstra): {execution_time:.6f} giây")
 
